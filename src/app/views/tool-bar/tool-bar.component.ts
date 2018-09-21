83e5d567e2f12fdf13cf07ee4bb9776bee0024d4
saumye <shavinathsaumye@gmail.com>
2018-09-22 03:34:10 +0530

    home button

--- src/app/views/tool-bar/tool-bar.component.ts
+++ src/app/views/tool-bar/tool-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-tool-bar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  getHome(){
+    this.router.navigateByUrl("");
   }
 
 }

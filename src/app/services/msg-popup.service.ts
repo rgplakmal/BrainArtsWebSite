07f9746d62d15d33c2e58528aca5b88c88f72ded
Prasanna Lakmal <rgplakmal@gmail.com>
2018-09-21 19:13:19 +0530

    add slick

---
+++ src/app/services/msg-popup.service.ts
@@ -0,0 +1,12 @@
+import { Injectable , Output , EventEmitter } from '@angular/core';
+
+@Injectable()
+export class MsgPopupService {
+  @Output() messagePopupEventEmitter: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  broadcastMessagePopupEventEmitter(response : any){
+    this.messagePopupEventEmitter.emit(response);
+  }
+}
